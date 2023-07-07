c3dd7ddd6adb1697e87a01e89fb0b387814e7028
Aleksandr Sykhover <Biotman@mail.ru>
2023-07-07 19:33:26 +0300

    Timer work

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,17 +1,14 @@
-const forms = () => {
-    const form = document.querySelectorAll('form'),
-            inputs = document.querySelectorAll('input'),
-            phoneInputs = document.querySelectorAll('input[name="user_phone"');
+import checkNumInputs from './checkNumInputs';
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        })
-    })
-            
+const forms = (state) => {
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name="user_phone"]');
+    
     const message = {
         loading: 'Загрузка...',
-        success: 'Успешно!',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
         failure: 'Что-то пошло не так...'
     };
 
@@ -28,8 +25,8 @@ const forms = () => {
     const clearInputs = () => {
         inputs.forEach(item => {
             item.value = '';
-        })
-    }
+        });
+    };
 
     form.forEach(item => {
         item.addEventListener('submit', (e) => {
@@ -38,25 +35,28 @@ const forms = () => {
             let statusMessage = document.createElement('div');
             statusMessage.classList.add('status');
             item.appendChild(statusMessage);
-            
+
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
                     console.log(res);
                     statusMessage.textContent = message.success;
                 })
-                .catch(() => {
-                    statusMessage.textContent = message.failure;
-                })
+                .catch(() => statusMessage.textContent = message.failure)
                 .finally(() => {
                     clearInputs();
                     setTimeout(() => {
                         statusMessage.remove();
                     }, 5000);
-                })
+                });
         });
-    })
+    });
 };
 
 export default forms;
